ef31374304244f30cbee2cd4e964df3fd1aa64a6
Joel Maciel <soldado25461@gmail.com>
2022-11-22 21:17:03 -0300

    feat: implementing product listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { ProductService } from './service/product.service';
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
+  products: Product[] = [];
+
   productForm = this.fb.group({
     id: [],
     name: [null, Validators.required],
@@ -20,6 +22,17 @@ export class AppComponent {
     private fb: FormBuilder,
     private productService: ProductService
   ) {}
+
+  findAllProducts() {
+    this.productService.findAll().subscribe({
+      next: (result) => {
+        this.products = result;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 
   createProduct(): Product {
     return {
@@ -37,7 +50,26 @@ export class AppComponent {
 
       this.productService.save(product).subscribe({
         next: (res) => {
+          this.findAllProducts();
+          this.productForm.reset();
           alert('Product saved successfully');
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    }
+  }
+
+  delete(product: Product) {
+    const confirmation = confirm(
+      'Are you sure you want to delete this product'
+    );
+    if (confirmation) {
+      const id = product.id ?? 0;
+      this.productService.delete(id).subscribe({
+        next: (result) => {
+          alert('Product deleted successfully');
         },
         error: (error) => {
           console.log(error);
